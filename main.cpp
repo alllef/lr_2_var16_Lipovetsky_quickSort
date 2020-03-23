e7d45afad2dc125f9e0dd2db9d31d6dd6ef99489
alllef <alekz29@gmail.com>
2020-03-23 19:59:39 +0200

    first working version of alghoritm

--- main.cpp
+++ main.cpp
@@ -6,8 +6,12 @@
 using namespace std;
 
 void quickSort(vector<int> &array, unsigned int iLeft, unsigned int iRight);
-
+void quickSortWithMedian(vector<int> &array, unsigned int iLeft, unsigned int iRight);
 unsigned int findMedian(vector<int> &array, unsigned int iLeft, unsigned int iRight);
+void print(vector<int> &array);
+void testQuickSort(vector<int> array);
+void testMedian(vector<int> array);
+void testMedianFactory();
 
 void print(vector<int> &array) {
     for (int item : array) {
@@ -18,7 +22,7 @@ void print(vector<int> &array) {
 
 void testQuickSort(vector<int> array) {
     print(array);
-    quickSort(array, 0, array.size() - 1);
+    quickSortWithMedian(array, 0, array.size() - 1);
     print(array);
 
     cout << endl;
@@ -40,9 +44,11 @@ void testMedianFactory() {
 }
 
 void quickSortFactory() {
-    // testQuickSort({1, 2, 3});
-    // testQuickSort({3, 2, 1});
+    testQuickSort({1, 2, 3});
+    testQuickSort({3, 2, 1});
     testQuickSort({2, 3, 1});
+    testQuickSort({5, 3, 6, 10, 11, 13, 17});
+    testQuickSort({-2, 37, 44, 56, 22, 34});
 
     //testQuickSort({1, 74, 567, 854, 3, 2, 78, 45, 23, 24, 56, 28});
     //testQuickSort({5,24, 45, 456, 78, 67, 1020, 20, 34});
@@ -69,28 +75,41 @@ void quickSort(vector<int> &array, unsigned int iLeft, unsigned int iRight) {
     const unsigned int leftPos = iLeft;
     const unsigned int rightPos = iRight;
 
-    unsigned int iPivot = findMedian(array, iLeft, iRight);
-    int pivot = array[iPivot];
-
-    while (iLeft < iRight) {
-
-        while (array[iLeft] < pivot && iLeft < iRight) {
-            iLeft++;
+    int pivot;
+    pivot = array[iLeft];
+    while (iLeft < iRight) // пока границы не сомкнутся
+    {
+        while ((array[iRight] >= pivot) && (iLeft < iRight))
+            iRight--; // сдвигаем правую границу пока элемент [right] больше [pivot]
+        if (iLeft != iRight) // если границы не сомкнулись
+        {
+            array[iLeft] = array[iRight]; // перемещаем элемент [right] на место разрешающего
+            iLeft++; // сдвигаем левую границу вправо
         }
-
-        while (array[iRight] > pivot && iLeft < iRight) {
-            iRight--;
+        while ((array[iLeft] <= pivot) && (iLeft < iRight))
+            iLeft++; // сдвигаем левую границу пока элемент [left] меньше [pivot]
+        if (iLeft != iRight) // если границы не сомкнулись
+        {
+            array[iRight] = array[iLeft]; // перемещаем элемент [left] на место [right]
+            iRight--; // сдвигаем правую границу вправо
         }
-
-        if (iLeft != iPivot && iRight != iPivot) swap(array, iLeft, iRight);
-        if(iLeft != iRight) iLeft++;
-        if(iLeft != iRight)iRight--;
     }
+    array[iLeft] = pivot;
+    pivot = iLeft;
+    iLeft = leftPos;
+    iRight = rightPos;
+    if (iLeft < pivot) // Рекурсивно вызываем сортировку для левой и правой части массива
+        quickSort(array, iLeft, pivot - 1);
+    if (iRight > pivot)
+        quickSort(array, pivot + 1, iRight);
+}
 
-    if (array[iRight] != pivot) swap(array, iPivot, iRight);
+void quickSortWithMedian(vector<int> &array, unsigned int iLeft, unsigned int iRight) {
+    unsigned int iPivot = findMedian(array, iLeft, iRight);
 
-    if (iLeft < iPivot) { quickSort(array, leftPos, iPivot - 1); }
-    if (iRight > iPivot) { quickSort(array, iPivot + 1, rightPos); }
+    swap(array, iPivot, iLeft);
+
+    quickSort(array, iLeft, iRight);
 }
 
 unsigned int findMedian(vector<int> &array, unsigned int iLeft, unsigned int iRight) {
@@ -109,3 +128,7 @@ unsigned int findMedian(vector<int> &array, unsigned int iLeft, unsigned int iRi
 
 }
 
+void initializeArray(vector<int> &array,){
+    for (int i = 0; i<SIZE; i++)
+        a[i] = rand() % 201 - 100;
+}
