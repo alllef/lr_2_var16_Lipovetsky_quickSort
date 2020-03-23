8b0902d50b0ea362c84e3188d9c58366d90bb3d0
alllef <alekz29@gmail.com>
2020-03-23 20:17:59 +0200

    same alghoritm, but cleaned from all unnecessary things

--- main.cpp
+++ main.cpp
@@ -6,59 +6,21 @@
 using namespace std;
 
 void quickSort(vector<int> &array, unsigned int iLeft, unsigned int iRight);
+
 void quickSortWithMedian(vector<int> &array, unsigned int iLeft, unsigned int iRight);
+
 unsigned int findMedian(vector<int> &array, unsigned int iLeft, unsigned int iRight);
+
 void print(vector<int> &array);
+
 void testQuickSort(vector<int> array);
-void testMedian(vector<int> array);
-void testMedianFactory();
 
-void print(vector<int> &array) {
-    for (int item : array) {
-        cout << setw(5) << item;
-    }
-    cout << endl;
-}
-
-void testQuickSort(vector<int> array) {
-    print(array);
-    quickSortWithMedian(array, 0, array.size() - 1);
-    print(array);
-
-    cout << endl;
-}
-
-void testMedian(vector<int> array) {
-    print(array);
-    cout << setw(5) << array[findMedian(array, 0, array.size() - 1)] << endl;
-}
-
-void testMedianFactory() {
-
-    testMedian({1, 2, 3});
-    testMedian({3, 2, 1});
-    testMedian({2, 3, 1});
-    testMedian({5, 10, 2, 7, 8});
-    testMedian({5, 10, 8, 7, 2});
-
-}
-
-void quickSortFactory() {
-    testQuickSort({1, 2, 3});
-    testQuickSort({3, 2, 1});
-    testQuickSort({2, 3, 1});
-    testQuickSort({5, 3, 6, 10, 11, 13, 17});
-    testQuickSort({-2, 37, 44, 56, 22, 34});
-
-    //testQuickSort({1, 74, 567, 854, 3, 2, 78, 45, 23, 24, 56, 28});
-    //testQuickSort({5,24, 45, 456, 78, 67, 1020, 20, 34});
-
-}
+void initializeArray(vector<int> &array);
 
 int main() {
+    vector<int> array(100);
+    testQuickSort(array);
 
-    // testMedianFactory();
-    quickSortFactory();
 
     return 0;
 }
@@ -77,28 +39,25 @@ void quickSort(vector<int> &array, unsigned int iLeft, unsigned int iRight) {
 
     int pivot;
     pivot = array[iLeft];
-    while (iLeft < iRight) // пока границы не сомкнутся
-    {
+    while (iLeft < iRight) {
         while ((array[iRight] >= pivot) && (iLeft < iRight))
-            iRight--; // сдвигаем правую границу пока элемент [right] больше [pivot]
-        if (iLeft != iRight) // если границы не сомкнулись
-        {
-            array[iLeft] = array[iRight]; // перемещаем элемент [right] на место разрешающего
-            iLeft++; // сдвигаем левую границу вправо
+            iRight--;
+        if (iLeft != iRight) {
+            array[iLeft] = array[iRight];
+            iLeft++;
         }
         while ((array[iLeft] <= pivot) && (iLeft < iRight))
-            iLeft++; // сдвигаем левую границу пока элемент [left] меньше [pivot]
-        if (iLeft != iRight) // если границы не сомкнулись
-        {
-            array[iRight] = array[iLeft]; // перемещаем элемент [left] на место [right]
-            iRight--; // сдвигаем правую границу вправо
+            iLeft++;
+        if (iLeft != iRight) {
+            array[iRight] = array[iLeft];
+            iRight--;
         }
     }
     array[iLeft] = pivot;
     pivot = iLeft;
     iLeft = leftPos;
     iRight = rightPos;
-    if (iLeft < pivot) // Рекурсивно вызываем сортировку для левой и правой части массива
+    if (iLeft < pivot)
         quickSort(array, iLeft, pivot - 1);
     if (iRight > pivot)
         quickSort(array, pivot + 1, iRight);
@@ -128,7 +87,29 @@ unsigned int findMedian(vector<int> &array, unsigned int iLeft, unsigned int iRi
 
 }
 
-void initializeArray(vector<int> &array,){
-    for (int i = 0; i<SIZE; i++)
-        a[i] = rand() % 201 - 100;
+void initializeArray(vector<int> &array) {
+    for (int i = 0; i < array.size(); i++)
+        array[i] = rand() % 201 - 100;
 }
+
+void print(vector<int> &array) {
+    for (int item = 0; item < array.size(); item++) {
+        if (item % 10 == 0)cout << endl;
+        cout << setw(5) << array[item];
+    }
+    cout << endl;
+}
+
+void testQuickSort(vector<int> array) {
+    initializeArray(array);
+    print(array);
+    quickSortWithMedian(array, 0, array.size() - 1);
+    print(array);
+
+    cout << endl;
+}
+
+
+
+
+
